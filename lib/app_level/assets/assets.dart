4fb32d8c3ab8fd4a8df0228e76674a65924396a4
aseem wangoo <aseemwangoo@gmail.com>
2020-03-21 23:43:43 +0800

    [C/Dev] Restructure-4

---
+++ lib/app_level/assets/assets.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart' show AssetImage;
+
+class WebAssets {
+  WebAssets._();
+
+  static const flareBkgrd = AssetImage('assets/animations/spaceman.flr');
+}
--- lib/app_level/home.dart
+++ lib/app_level/home.dart
@@ -1,3 +1,6 @@
+import 'package:experiments_with_web/app_level/widgets/desktop/bg_widget.dart';
+import 'package:experiments_with_web/app_level/widgets/desktop/option_name.dart';
+
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -5,8 +8,38 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Placeholder(),
+    //
+
+    return Scaffold(
+      body: SafeArea(
+        child: BgWidget(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    OptionButton(
+                      buttonText: 'Flipping',
+                      onTap: () {},
+                    ),
+                    OptionButton(
+                      buttonText: 'Inking',
+                      onTap: () {},
+                    ),
+                    OptionButton(
+                      buttonText: 'LiquidCards',
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
