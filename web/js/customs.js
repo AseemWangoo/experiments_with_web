e6667d657e8a0df8f0200e314c0ca702ad196e62
aseem wangoo <aseemwangoo@gmail.com>
2020-04-12 17:34:14 +0800

    [C/Location] Include loc. fxn

--- web/js/customs.js
+++ web/js/customs.js
@@ -1,3 +1,23 @@
 function webAlert(text) {
     alert(text);
 }
+
+function detectUserLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+        status.textContent = 'Geolocation is not supported by your browser';
+    }
+}
+
+function success(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    console.log('LAT', latitude);
+    console.log('LONG', longitude);
+}
+
+function error() {
+    status.textContent = 'Unable to get your location';
+}
