227c3b69b7521315b4cc09c7efef8f508ba156de
aseem wangoo <aseemwangoo@gmail.com>
2020-04-12 18:23:36 +0800

    [C/Location] Include display of coordinates

--- lib/location/location.dart
+++ lib/location/location.dart
@@ -9,10 +9,18 @@ import 'package:experiments_with_web/location/utilities/stringify.dart';
 import 'package:flutter/material.dart';
 import 'package:js/js.dart';
 
-class LocationScreen extends StatelessWidget {
+class LocationScreen extends StatefulWidget {
   const LocationScreen({Key key}) : super(key: key);
 
   static const _obj = '{ name: "AseemWangoo", location: "Earth" }';
+
+  @override
+  _LocationScreenState createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+  double _latitude = 0.0;
+  double _longitude = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +33,7 @@ class LocationScreen extends StatelessWidget {
           children: <Widget>[
             OutlineButton(
               onPressed: () {
-                final _res = stringify(_obj);
+                final _res = stringify(LocationScreen._obj);
                 window.console.log(_res);
               },
               child: const Text('Call JS Stringify'),
@@ -50,26 +58,21 @@ class LocationScreen extends StatelessWidget {
             OutlineButton(
               onPressed: () {
                 getCurrentPosition(allowInterop((pos) {
-                  success(pos);
+                  // print('LAT >>> ${pos.coords.latitude}');
+                  // print('LONG >>> ${pos.coords.longitude}');
+                  setState(() {
+                    _latitude = pos.coords.latitude;
+                    _longitude = pos.coords.longitude;
+                  });
                 }));
               },
               child: const Text('Mozilla GeoLocationAPI'),
             ),
+            Text('LAT : $_latitude'),
+            Text('LONG : $_longitude'),
           ],
         ),
       ),
     );
   }
-
-  dynamic success(GeolocationPosition pos) {
-    try {
-      print('HELLLO');
-      print(pos.coords.latitude);
-      print(pos.coords.longitude);
-    } catch (exc) {
-      print('ERROR WHILE FETCHING LOCATION : ${exc.toString()}');
-    }
-  }
 }
-
-// CANNOT WORK ON WEB SERVER
