a6f04ed88cfcee2fe6a9a614f0e9978739dafceb
aseem wangoo <aseemwangoo@gmail.com>
2020-04-12 12:08:17 +0800

    [C/Location] Include JS pkg

--- lib/location/location.dart
+++ lib/location/location.dart
@@ -1,4 +1,8 @@
+import 'dart:html';
+
 import 'package:experiments_with_web/app_level/widgets/desktop/custom_scaffold.dart';
+import 'package:experiments_with_web/location/utilities/helpers.dart';
+
 import 'package:flutter/material.dart';
 
 class LocationScreen extends StatelessWidget {
@@ -7,6 +11,22 @@ class LocationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //
-    return const CustomScaffold();
+    return CustomScaffold(
+      child: Column(
+        children: <Widget>[
+          FlatButton(
+            onPressed: () {
+              final _res =
+                  stringify('{ name: "John", age: 30, city: "New York" }');
+              // window.console.log(_res);
+              print(_res);
+            },
+            child: Text('Call JS Stringify'),
+          ),
+        ],
+      ),
+    );
   }
 }
+
+// CANNOT WORK ON WEB SERVER
