cddfdc65e88ab714c3281c595a66b93ae7accef9
aseemwangooAI <aseem.wangoo@braiyt.com>
2020-04-19 16:49:28 +0800

    [C/ML] Display Val

--- lib/ml/ml.dart
+++ lib/ml/ml.dart
@@ -5,8 +5,15 @@ import 'package:experiments_with_web/ml/utilities/linear_ml.dart';
 
 import 'package:flutter/material.dart';
 
-class MLScreen extends StatelessWidget {
+class MLScreen extends StatefulWidget {
   const MLScreen({Key key}) : super(key: key);
+
+  @override
+  _MLScreenState createState() => _MLScreenState();
+}
+
+class _MLScreenState extends State<MLScreen> {
+  num _predictedValue = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -16,16 +23,18 @@ class MLScreen extends StatelessWidget {
       titleText: 'ML on Web',
       child: Center(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             OutlineButton(
               onPressed: () async {
                 final _val =
                     await jsutil.promiseToFuture<num>(linearModel('text'));
 
-                print('From Dart $_val');
+                setState(() => _predictedValue = _val);
               },
               child: const Text('Linear Model'),
             ),
+            Text('Predicted Value $_predictedValue'),
           ],
         ),
       ),
