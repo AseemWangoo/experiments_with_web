c1fa7d816e67aca01701760d0e87bbed97fa49eb
aseem wangoo <aseemwangoo@gmail.com>
2020-04-26 14:12:49 +0800

    [C/Game] Init game

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -1,16 +1,44 @@
-import 'package:experiments_with_web/app_level/widgets/desktop/custom_scaffold.dart';
+import 'package:flame/game/game.dart';
 
 import 'package:flutter/material.dart';
 
-class GameScreen extends StatelessWidget {
+class GameScreen extends StatefulWidget {
   const GameScreen({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    //
+  _GameScreenState createState() => _GameScreenState();
+}
 
-    return const CustomScaffold(
-      titleText: 'Game in Web',
+class _GameScreenState extends State<GameScreen> {
+  final _game = _BoxGame();
+
+  @override
+  Widget build(BuildContext context) => _game.widget;
+}
+
+class _BoxGame extends Game {
+  Size _screenSize;
+
+  @override
+  void render(Canvas canvas) {
+    final _bgRect = Rect.fromLTWH(
+      0.0,
+      0.0,
+      _screenSize.width,
+      _screenSize.height,
     );
+
+    final _bgPaint = Paint()..color = Colors.deepOrange;
+
+    canvas.drawRect(_bgRect, _bgPaint);
+  }
+
+  @override
+  void update(double t) {}
+
+  @override
+  void resize(Size size) {
+    _screenSize = size;
+    super.resize(size);
   }
 }
