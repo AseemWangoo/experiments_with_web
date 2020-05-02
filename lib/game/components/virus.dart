af218fd25cb471223f35df01dc6e97df612f7150
aseem wangoo <aseemwangoo@gmail.com>
2020-05-02 11:56:52 +0800

    [C/Game] Move virus in diff. directions

--- lib/game/components/virus.dart
+++ lib/game/components/virus.dart
@@ -5,11 +5,15 @@ import 'package:flame/sprite.dart';
 import 'package:flutter/foundation.dart';
 
 import 'package:experiments_with_web/game/game.dart';
+import 'package:experiments_with_web/game/utilities/helpers.dart';
+import 'package:experiments_with_web/game/utilities/constants.dart';
 
 class Virus {
   Virus({
     @required this.gameTime,
-  });
+  }) {
+    setupVirusLocation;
+  }
 
   final GameTime gameTime;
 
@@ -20,6 +24,8 @@ class Virus {
   List<Sprite> movingVirusSprite;
   Sprite deadVirusSprite;
   double movingSpriteIndex = 0;
+  double get virusSpeed => gameTime.tileSize * 3;
+  Offset targetVirusLocation;
 
   void render(Canvas c) {
     final _inflatedRect = virusRect.inflate(14.0);
@@ -46,6 +52,21 @@ class Virus {
       if (movingSpriteIndex >= 2) {
         movingSpriteIndex -= 2;
       }
+
+      // MOVING LOGIC
+      final _virusDistance = virusSpeed * t;
+      final _targetOffset =
+          targetVirusLocation - Offset(virusRect.left, virusRect.top);
+
+      if (_virusDistance < _targetOffset.distance) {
+        final _updateTargetOffset =
+            Offset.fromDirection(_targetOffset.direction, _virusDistance);
+
+        virusRect = virusRect.shift(_updateTargetOffset);
+      } else {
+        virusRect = virusRect.shift(_targetOffset);
+        setupVirusLocation;
+      }
     }
   }
 
@@ -56,5 +77,19 @@ class Virus {
 
     // ADDS THE VIRUS AGAIN..
     gameTime.createVirus();
+  }
+
+  void get setupVirusLocation {
+    final _rndDouble = GameHelpers.randomize();
+
+    final _x = _rndDouble *
+        (gameTime.screenSize.width -
+            (gameTime.tileSize * GameUtils.maxVirusSize));
+
+    final _y = _rndDouble *
+        (gameTime.screenSize.height -
+            (gameTime.tileSize * GameUtils.maxVirusSize));
+
+    targetVirusLocation = Offset(_x, _y);
   }
 }
