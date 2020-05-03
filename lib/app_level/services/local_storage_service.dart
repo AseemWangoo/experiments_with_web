7967551ac5d77920125bc4c872d2c057a1690d41
aseem wangoo <aseemwangoo@gmail.com>
2020-05-03 10:58:29 +0800

    [C/Game] Include localstorage service

---
+++ lib/app_level/services/local_storage_service.dart
@@ -0,0 +1,30 @@
+import 'dart:html' as html;
+
+class LocalStorageService {
+  LocalStorageService();
+
+  void saveKeyValue(String key, String value) {
+    final _localStorage = html.window.localStorage;
+
+    if (_localStorage.containsKey(key)) {
+      _localStorage.update(key, (value) => value);
+    } else {
+      _localStorage.putIfAbsent(key, () => value);
+    }
+  }
+
+  void get clear {
+    final _localStorage = html.window.localStorage;
+    _localStorage.clear();
+  }
+
+  void removeKey(String key) {
+    final _localStorage = html.window.localStorage;
+    _localStorage.remove(key);
+  }
+
+  Future<String> fetchValue(String key) async {
+    final _localStorage = html.window.localStorage;
+    return _localStorage['$key'];
+  }
+}
