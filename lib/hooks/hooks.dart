0a2573205924e2d769a9f16f36fde543e7b8928e
aseem wangoo <aseemwangoo@gmail.com>
2020-06-17 22:53:42 +0800

    [C/Hooks] Save progress

--- lib/hooks/hooks.dart
+++ lib/hooks/hooks.dart
@@ -1,5 +1,6 @@
 import 'package:experiments_with_web/app_level/styles/themes.dart';
 import 'package:experiments_with_web/app_level/widgets/desktop/centered_form.dart';
+import 'package:experiments_with_web/app_level/widgets/desktop/custom_dialog.dart';
 import 'package:experiments_with_web/app_level/widgets/desktop/custom_input_field.dart';
 import 'package:experiments_with_web/app_level/widgets/desktop/custom_scaffold.dart';
 import 'package:experiments_with_web/hooks/models/suggestion.dart';
@@ -15,13 +16,21 @@ class HooksScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
     //
-    debugPrint('IM BUILDING');
 
-    final _model = useMemoized(
-      () => Suggestion(personName: 'as'),
+    final _field = useTextEditingController.fromValue(
+      TextEditingValue.empty,
     );
 
-    debugPrint('IM ${_model.personName}');
+    final update = useValueListenable(_field);
+
+    useEffect(() {
+      _field.text = update.text;
+      return null;
+    }, [update]);
+
+    final _onSavePressed = useState(false);
+
+    debugPrint('IM BUILDING');
 
     return CustomScaffold(
       titleText: HookScreenConstants.hookTitle,
@@ -31,16 +40,39 @@ class HooksScreen extends HookWidget {
             HookScreenConstants.formTitle,
             style: AppTheme.darkTheme.textTheme.headline4,
           ),
-          HookBuilder(
-            builder: (_) {
-              final _state = useState('');
-              return CustomInputField(
-                onChanged: (val) {
-                  debugPrint('>>> $val');
-                  _state.value = val;
-                },
-              );
+          CustomInputField(
+            showError: _onSavePressed.value && _field.text.isEmpty,
+            onChanged: (val) {
+              debugPrint('>>> $val');
             },
+            hintText: HookScreenConstants.personFieldHint,
+            labelText: HookScreenConstants.personLabel,
+            textController: _field,
+          ),
+          RaisedButton.icon(
+            onPressed: () {
+              final _model = Suggestion(personName: _field.text);
+              _onSavePressed.value = true;
+
+              debugPrint('1 ${_onSavePressed.value}');
+              debugPrint('2 ${_field.text.isEmpty}');
+
+              if (_onSavePressed.value) {
+                showDialog(
+                  context: context,
+                  builder: (_) => CustomDialog(
+                    child: Column(
+                      children: [
+                        Text('${_model.toJson()}'),
+                      ],
+                    ),
+                  ),
+                );
+              }
+              _field.text;
+            },
+            icon: Icon(Icons.save),
+            label: Text('Save'),
           ),
         ],
       ),
--- lib/hooks/utilities/constants.dart
+++ lib/hooks/utilities/constants.dart
@@ -3,4 +3,10 @@ class HookScreenConstants {
 
   static const String hookTitle = 'Hands on With Flutter Hooks';
   static const String formTitle = 'Tutorial Suggestions';
+
+  // FIELDS
+  static const String personLabel = 'Your Name';
+  static const String personFieldHint = 'Steve';
+  static const String personHint =
+      'Will be used to shout out, if the tutorial gets selected';
 }
