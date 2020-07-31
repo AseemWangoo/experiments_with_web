22f8834eb8082ff6ea87b4abe103911c66729c9a
aseem wangoo <aseemwangoo@gmail.com>
2020-07-31 16:37:43 +0800

    [C/Dev] Bind menu

--- lib/app_level/widgets/desktop/widget_screen.dart
+++ lib/app_level/widgets/desktop/widget_screen.dart
@@ -1,8 +1,6 @@
-import 'package:experiments_with_web/app_level/utilities/screen_size.dart';
-
 import 'package:flutter/material.dart';
 
-class WidgetScreener extends StatefulWidget {
+class WidgetScreener extends StatelessWidget {
   const WidgetScreener({
     Key key,
     @required this.child,
@@ -15,60 +13,14 @@ class WidgetScreener extends StatefulWidget {
   final double scrollbarWidth;
 
   @override
-  _WidgetScreenerState createState() => _WidgetScreenerState();
-}
-
-class _WidgetScreenerState extends State<WidgetScreener> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    //
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Material(
         elevation: 4.0,
         child: AspectRatio(
-          aspectRatio: widget.ratio,
-          child: widget.child,
-        ),
-      ),
-    );
-  }
-}
-
-class _CustomScrollBar extends StatelessWidget {
-  const _CustomScrollBar({
-    Key key,
-    this.controller,
-    this.scrollbarWidth = 20.0,
-  }) : super(key: key);
-
-  final double scrollbarWidth;
-  final ScrollController controller;
-
-  @override
-  Widget build(BuildContext context) {
-    final _height = ScreenQueries.instance.height(context);
-    final _width = ScreenQueries.instance.width(context);
-
-    return Container(
-      alignment: Alignment.centerRight,
-      color: Colors.yellow,
-      height: _height,
-      margin: EdgeInsets.only(left: _width - scrollbarWidth),
-      width: scrollbarWidth,
-      child: Container(
-        alignment: Alignment.topCenter,
-        child: GestureDetector(
-          child: Container(
-            height: 40.0,
-            width: 15.0,
-            child: Text('Hii'),
-          ),
+          aspectRatio: ratio,
+          child: child,
         ),
       ),
     );
--- lib/codepenz/codepen.dart
+++ lib/codepenz/codepen.dart
@@ -1,19 +1,18 @@
-import 'package:experiments_with_web/app_level/assets/assets.dart';
+import 'package:experiments_with_web/app_level/extensions/widget_extension.dart';
 import 'package:experiments_with_web/app_level/styles/colors.dart';
-import 'package:experiments_with_web/app_level/widgets/desktop/custom_scaffold.dart';
-import 'package:experiments_with_web/app_level/widgets/desktop/image_loader.dart';
+import 'package:experiments_with_web/app_level/utilities/screen_size.dart';
+import 'package:experiments_with_web/app_level/widgets/desktop/custom_scrollbar.dart';
 import 'package:experiments_with_web/app_level/widgets/desktop/item_grid_placer.dart';
 import 'package:experiments_with_web/app_level/widgets/desktop/row_spacer.dart';
+import 'package:experiments_with_web/app_level/widgets/desktop/scroller.dart';
 import 'package:experiments_with_web/app_level/widgets/desktop/sliver_scaffold.dart';
-import 'package:experiments_with_web/app_level/widgets/desktop/widget_iframer.dart';
 import 'package:experiments_with_web/app_level/widgets/desktop/widget_screen.dart';
-import 'package:experiments_with_web/codepenz/implicitly.dart';
-import 'package:experiments_with_web/codepenz/stack.dart';
 
 import 'package:flutter/material.dart';
 
+import 'implicitly.dart';
 import 'layout.dart';
-import 'painter.dart';
+import 'stack.dart';
 
 class CodepenScreen extends StatefulWidget {
   const CodepenScreen({Key key}) : super(key: key);
@@ -31,22 +30,76 @@ class _CodepenScreenState extends State<CodepenScreen> {
     super.initState();
   }
 
+  double get _height => ScreenQueries.instance.height(context);
+
   @override
   Widget build(BuildContext context) {
-    // return CustomScaffold(
-    //   showAppBar: false,
-    //   child: WidgetScreener(
-    //     child: ImplicitlyAseem(),
-    //   ),
-    // );
-    return SliverScaffold(
+    return CustomScrollbar(
       controller: _controller,
-      children: [
-        Placeholder(),
-        Placeholder(),
-      ],
-      menu: _Menu(),
-      statusBarChild: _OverTheMenu(),
+      child: SliverScaffold(
+        controller: _controller,
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            height: _height * 0.85,
+            child: WidgetScreener(
+              child: SimpleSliverScaffold(
+                menu: _DemoMenu(),
+                children: [
+                  _DemoContainer(color: const Color(0xFF36311f)),
+                  _DemoContainer(color: const Color(0xFF7d8ca3)),
+                  _DemoContainer(color: const Color(0xFF4f3130)),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.topRight,
+            height: _height * 0.85,
+            child: WidgetScreener(
+              child: SliverScaffold(
+                controller: null,
+                menu: _DemoMenu(menuText: 'Disappear-header Sliver'),
+                children: [
+                  _DemoContainer(color: const Color(0xFFd8bd8a)),
+                  _DemoContainer(color: const Color(0xFFaa5042)),
+                  _DemoContainer(color: const Color(0xFF753742)),
+                ],
+                statusBarChild: _OverTheMenu(),
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.topLeft,
+            height: _height * 0.85,
+            child: WidgetScreener(
+              child: SliverAppbarScaffold(
+                children: [
+                  _DemoContainer(color: const Color(0xFF8963ba)),
+                  _DemoContainer(color: const Color(0xFFaa5042)),
+                  _DemoContainer(color: const Color(0xFFff9f1c)),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.topRight,
+            height: _height * 0.85,
+            child: WidgetScreener(
+              child: SliverPaddingScaffold(),
+            ),
+          ),
+          ItemsScroller(
+            children: [
+              WidgetScreener(child: ImplicitlyAseem()),
+              WidgetScreener(child: AseemLayouted()),
+              WidgetScreener(child: AseemStack()),
+            ],
+          ),
+        ],
+        menu: _Menu(controller: _controller),
+        statusBarChild: _OverTheMenu(),
+      ),
     );
   }
 
@@ -57,24 +110,92 @@ class _CodepenScreenState extends State<CodepenScreen> {
   }
 }
 
-class _Menu extends StatelessWidget {
-  const _Menu({Key key}) : super(key: key);
+class _DemoContainer extends StatelessWidget {
+  const _DemoContainer({Key key, this.color = const Color(0xFFf75c03)})
+      : super(key: key);
+
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
+    var _height = ScreenQueries.instance.height(context);
+
+    return Container(color: color, height: _height * 0.4);
+  }
+}
+
+class _DemoMenu extends StatelessWidget {
+  const _DemoMenu({
+    Key key,
+    this.color = const Color(0xFFf75c03),
+    this.menuText = 'Simple Sliver',
+  }) : super(key: key);
+
+  final Color color;
+  final String menuText;
+
+  TextStyle get _textStyle => TextStyle(
+        color: Colors.white,
+        fontSize: 20.0,
+        decoration: TextDecoration.none,
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
+      child: ItemGridPlacer(
+        child: Text(menuText, style: _textStyle, textAlign: TextAlign.center),
+      ),
+    );
+  }
+}
+
+class _Menu extends StatelessWidget {
+  const _Menu({Key key, this.controller}) : super(key: key);
+
+  final ScrollController controller;
+
+  TextStyle get _textStyle => TextStyle(
+        color: Colors.white,
+        fontSize: 20.0,
+        decoration: TextDecoration.none,
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    //
+    var _height = ScreenQueries.instance.height(context);
+
     return DecoratedBox(
       decoration: BoxDecoration(color: AppColors.brandColor),
       child: ItemGridPlacer(
         child: RowSpacer(
-          children: const <Widget>[
-            Text('1'),
-            Text('1'),
-            Text('1'),
-            Text('1'),
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text('Simple Sliver', style: _textStyle)
+                .clickable(() => _scrollToItem(controller, 0.0)),
+            Text('Sliver with Header', style: _textStyle)
+                .clickable(() => _scrollToItem(controller, _height)),
+            Text('Sliver Appbar', style: _textStyle)
+                .clickable(() => _scrollToItem(controller, 2 * _height)),
+            Text('Others', style: _textStyle)
+                .clickable(() => _scrollToItem(controller, 3 * _height)),
+            Text('Codepens', style: _textStyle)
+                .clickable(() => _scrollToItem(controller, 4 * _height)),
           ],
           spacerWidget: const SizedBox(width: 24.0),
         ),
       ),
+    );
+  }
+
+  void _scrollToItem(ScrollController _controller, [double multiplier = 0.0]) {
+    _controller.animateTo(
+      multiplier,
+      duration: const Duration(seconds: 1),
+      curve: Curves.easeInOut,
     );
   }
 }
