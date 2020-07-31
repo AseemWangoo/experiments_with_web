320905da6ac173229553193529380c820783c077
aseem wangoo <aseemwangoo@gmail.com>
2020-07-31 10:34:35 +0800

    [C/Dev] Finalise scrollbar

--- lib/app_level/widgets/desktop/custom_scrollbar.dart
+++ lib/app_level/widgets/desktop/custom_scrollbar.dart
@@ -38,6 +38,7 @@ class _CustomScrollbarState extends State<CustomScrollbar> {
 
   Size get _screenSize => MediaQuery.of(context).size;
   double get _scrollerHeight => _screenSize.height * widget.heightFraction;
+
   double get _distanceFromTop => widget.controller.hasClients
       ? ((_screenSize.height *
               _scrollPosition /
@@ -47,7 +48,8 @@ class _CustomScrollbarState extends State<CustomScrollbar> {
               widget.controller.position.maxScrollExtent))
       : 0;
 
-  static const _kDuration = Duration(milliseconds: 300);
+  static const _kDuration = Duration(milliseconds: 900);
+  static const _kDisapperScrollDuration = Duration(seconds: 5);
 
   @override
   void initState() {
@@ -77,38 +79,93 @@ class _CustomScrollbarState extends State<CustomScrollbar> {
   @override
   Widget build(BuildContext context) {
     //
-    return Stack(
-      children: [
-        widget.child,
-        AnimatedOpacity(
-          duration: _kDuration,
-          opacity: _opacity,
-          child: Container(
-            alignment: Alignment.centerRight, // TRY REMOVING
-            color: widget.backgroundColor,
-            height: _screenSize.height,
-            margin: EdgeInsets.only(
-              left:
-                  _screenSize.width - widget.scrollBarWidth + 2, //PLAY WITH +2
-            ),
-            width: widget.scrollBarWidth + 2,
-            child: Align(
-              alignment: Alignment.topCenter,
-              child: GestureDetector(
-                child: Container(
-                  height: _scrollerHeight,
-                  width: widget.scrollBarWidth,
-                  margin: EdgeInsets.fromLTRB(1.0, _distanceFromTop, 1.0, 0),
-                  decoration: BoxDecoration(
-                    color: widget.color,
-                    borderRadius: BorderRadius.all(Radius.circular(3.0)),
+
+    return NotificationListener<ScrollNotification>(
+      onNotification: (notification) {
+        if (notification.depth == 0) {
+          if (notification is ScrollUpdateNotification) {
+            timer?.cancel();
+            setState(() => _isUpdating = true);
+          } else {
+            _onTapCancel();
+          }
+        }
+        return true;
+      },
+      child: Stack(
+        children: [
+          widget.child,
+          AnimatedOpacity(
+            duration: _kDuration,
+            opacity: _opacity,
+            child: Container(
+              alignment: Alignment.centerRight, // TRY REMOVING
+              color: widget.backgroundColor,
+              height: _screenSize.height,
+              margin: EdgeInsets.only(
+                left: _screenSize.width -
+                    widget.scrollBarWidth +
+                    2, //PLAY WITH +2
+              ),
+              width: widget.scrollBarWidth + 2,
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: GestureDetector(
+                  child: Container(
+                    height: _scrollerHeight,
+                    width: widget.scrollBarWidth,
+                    margin: EdgeInsets.fromLTRB(1.0, _distanceFromTop, 1.0, 0),
+                    decoration: BoxDecoration(
+                      color: widget.color,
+                      borderRadius: BorderRadius.all(Radius.circular(3.0)),
+                    ),
                   ),
+                  onTapCancel: _onTapCancel,
+                  onTapDown: _onTapDown,
+                  onVerticalDragUpdate: _onVerticalDragUpdate,
                 ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
+  }
+
+  void _onTapCancel() {
+    timer = Timer(_kDisapperScrollDuration, () {
+      setState(() => _isUpdating = false);
+    });
+  }
+
+  void _onTapDown(TapDownDetails details) {
+    timer?.cancel();
+    setState(() => _isUpdating = true);
+  }
+
+  void _onVerticalDragUpdate(DragUpdateDetails details) {
+    final _maxScrollExtent = widget.controller.position.maxScrollExtent;
+    final _posY = details.globalPosition.dy;
+
+    final _val = _posY +
+        _posY * (_scrollPosition / _maxScrollExtent) -
+        (_scrollerHeight * _scrollPosition / _maxScrollExtent);
+
+    widget.controller.position.jumpTo(_val);
+
+    // CHECK IF THE SCROLL IS REACHED AT THE END...
+    setState(() {
+      if (_posY >= 0 && _scrollPosition <= _maxScrollExtent) {
+        _scrollPosition = _posY +
+            _posY * (_scrollPosition / _maxScrollExtent) -
+            (_scrollerHeight * _scrollPosition / _maxScrollExtent);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
   }
 }
