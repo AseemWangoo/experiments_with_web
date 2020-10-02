7352b60c784beb01877b1168d76ab00298d23e70
aseem wangoo <aseemwangoo@gmail.com>
2020-10-02 17:45:47 +0800

    [C/Home] REvamp fav

--- lib/home/widgets/option_fav.dart
+++ lib/home/widgets/option_fav.dart
@@ -2,6 +2,8 @@ import 'package:experiments_with_web/app_level/extensions/textstyle_extension.da
 import 'package:experiments_with_web/app_level/extensions/widget_extension.dart';
 import 'package:experiments_with_web/app_level/models/articles/articles.dart';
 import 'package:experiments_with_web/app_level/widgets/desktop/parallax_btn.dart';
+import 'package:experiments_with_web/app_level/widgets/desktop/sliver_scaffold.dart';
+import 'package:experiments_with_web/home/widgets/top_nav.dart';
 import 'package:experiments_with_web/locator.dart';
 import 'package:experiments_with_web/app_level/services/hive/hive_operations.dart';
 
@@ -11,35 +13,62 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 import 'grid_view.dart';
 
-class OptionFav extends StatelessWidget {
+class OptionFav extends StatefulWidget {
   const OptionFav({Key key}) : super(key: key);
 
   static final _hiveService = locator<HiveOperationsService>();
 
   @override
+  _OptionFavState createState() => _OptionFavState();
+}
+
+class _OptionFavState extends State<OptionFav> {
+  ScrollController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = ScrollController();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final _nav = Navigator.of(context);
 
-    return ValueListenableBuilder(
-      builder: (_, Box<ArticlesModel> model, child) {
-        //
+    return SimpleSliverScaffold(
+      controller: _controller,
+      minHeight: 120.0,
+      maxHeight: 120.0,
+      menu: const TopNavBar(),
+      children: [
+        ValueListenableBuilder(
+          builder: (_, Box<ArticlesModel> model, child) {
+            //
 
-        if (model.values.isEmpty) {
-          return Center(
-            child: Text(
-              '🥺 🥺 No favorites !!',
-              style: Theme.of(context).textTheme.bodyText1.c(Colors.white),
-            ),
-          );
-        }
+            if (model.values.isEmpty) {
+              return Center(
+                child: Text(
+                  '🥺 🥺 No favorites !!',
+                  style: Theme.of(context).textTheme.bodyText1.c(Colors.white),
+                ),
+              );
+            }
 
-        return HomeGridView(
-          children: _displayOptions(_nav, model),
-        );
-      },
-      valueListenable: _hiveService.favBox.listenable(),
-      child: const Placeholder(),
+            return HomeGridView(
+              children: _displayOptions(_nav, model),
+            );
+          },
+          valueListenable: OptionFav._hiveService.favBox.listenable(),
+          child: const Placeholder(),
+        ),
+      ],
     );
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
   }
 
   List<Widget> _displayOptions(NavigatorState nav, Box<ArticlesModel> model) {
--- lib/home/widgets/option_home.dart
+++ lib/home/widgets/option_home.dart
@@ -1,22 +1,17 @@
-import 'package:experiments_with_web/app_level/assets/assets.dart';
 import 'package:experiments_with_web/app_level/constants/constants.dart';
-import 'package:experiments_with_web/app_level/extensions/textstyle_extension.dart';
 import 'package:experiments_with_web/app_level/extensions/widget_extension.dart';
 import 'package:experiments_with_web/app_level/models/articles/articles.dart';
 import 'package:experiments_with_web/app_level/services/hive/hive_operations.dart';
-import 'package:experiments_with_web/app_level/services/linker_service.dart';
-import 'package:experiments_with_web/app_level/styles/colors.dart';
-import 'package:experiments_with_web/app_level/widgets/desktop/image_loader.dart';
 import 'package:experiments_with_web/app_level/widgets/desktop/parallax_btn.dart';
 import 'package:experiments_with_web/app_level/widgets/desktop/sliver_scaffold.dart';
 
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import '../../locator.dart';
 import 'grid_view.dart';
+import 'top_nav.dart';
 
 class OptionHome extends StatefulWidget {
   const OptionHome({Key key}) : super(key: key);
@@ -27,7 +22,6 @@ class OptionHome extends StatefulWidget {
 
 class _OptionHomeState extends State<OptionHome> {
   ScrollController _controller;
-  static final _linkService = locator<LinkerService>();
   static final _hiveService = locator<HiveOperationsService>();
 
   @override
@@ -40,7 +34,6 @@ class _OptionHomeState extends State<OptionHome> {
   Widget build(BuildContext context) {
     //
     final _nav = Navigator.of(context);
-    final _whiteStyle = Theme.of(context).textTheme.headline6.c(Colors.white);
 
     return SimpleSliverScaffold(
       controller: _controller,
@@ -54,74 +47,7 @@ class _OptionHomeState extends State<OptionHome> {
           valueListenable: _hiveService.favBox.listenable(),
         ),
       ],
-      menu: Container(
-        color: AppColors.navColor,
-        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 24),
-        child: Row(
-          children: [
-            ImageWidgetPlaceholder(image: WebAssets.logo).clickable(() {
-              _controller.animateTo(
-                0.0,
-                duration: const Duration(milliseconds: 300),
-                curve: Curves.easeInOut,
-              );
-            }),
-            const Spacer(),
-            Column(
-              children: <Widget>[
-                Text(ApplevelConstants.homeTitle, style: _whiteStyle),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    IconButton(
-                      color: Colors.white,
-                      icon: FaIcon(FontAwesomeIcons.youtube),
-                      iconSize: 20.0,
-                      padding: EdgeInsets.zero,
-                      onPressed: () =>
-                          _linkService.openLink(BrandLinks.youtube),
-                    ),
-                    IconButton(
-                      color: Colors.white,
-                      iconSize: 20.0,
-                      icon: FaIcon(FontAwesomeIcons.medium),
-                      padding: EdgeInsets.zero,
-                      onPressed: () => _linkService.openLink(BrandLinks.medium),
-                    ),
-                    IconButton(
-                      iconSize: 20.0,
-                      color: Colors.white,
-                      icon: FaIcon(FontAwesomeIcons.chrome),
-                      padding: EdgeInsets.zero,
-                      onPressed: () =>
-                          _linkService.openLink(BrandLinks.website),
-                    ),
-                    IconButton(
-                      iconSize: 20.0,
-                      color: Colors.white,
-                      icon: FaIcon(FontAwesomeIcons.dev),
-                      padding: EdgeInsets.zero,
-                      onPressed: () => _linkService.openLink(BrandLinks.devTo),
-                    ),
-                    IconButton(
-                      iconSize: 20.0,
-                      color: Colors.white,
-                      icon: FaIcon(FontAwesomeIcons.github),
-                      padding: EdgeInsets.zero,
-                      onPressed: () => _linkService.openLink(BrandLinks.github),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            const Spacer(),
-            Text(
-              ApplevelConstants.supportTitle,
-              style: _whiteStyle,
-            ).clickable(() => _linkService.openLink(BrandLinks.support)),
-          ],
-        ),
-      ),
+      menu: const TopNavBar(),
     );
   }
 
