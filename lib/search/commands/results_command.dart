2a62c069b7101a46a07bf553865b81e2d8131012
aseem wangoo <aseemwangoo@gmail.com>
2020-10-11 18:10:32 +0800

    [C/Article] Find the article details

--- lib/search/commands/results_command.dart
+++ lib/search/commands/results_command.dart
@@ -1,7 +1,19 @@
 import 'package:experiments_with_web/app_level/commands/base_command.dart';
+import 'package:experiments_with_web/app_level/constants/constants.dart';
+import 'package:experiments_with_web/app_level/models/articles/articles.dart';
 
 class SearchResultCommand extends BaseCommand {
+  SearchResultCommand() {
+    _combinedInputList;
+  }
+
   void tap(String result) {
     print('You clicked $result');
+    var article =
+        _combinedInputList.where((item) => item.articleName == result).first;
+
+    print(article.toJson());
   }
+
+  List<ArticlesModel> get _combinedInputList => OptionsModel.options();
 }
--- lib/search/commands/search_command.dart
+++ lib/search/commands/search_command.dart
@@ -1,12 +1,10 @@
 import 'package:experiments_with_web/app_level/commands/base_command.dart';
 import 'package:experiments_with_web/app_level/constants/constants.dart';
-import 'package:experiments_with_web/app_level/models/articles/articles.dart';
 import 'package:experiments_with_web/app_level/widgets/desktop/change_notifier.dart';
 import 'package:experiments_with_web/search/string_search.dart';
 
 class SearchCommand extends GenericNotifier with BaseCommand {
   SearchCommand() {
-    _combinedInputList;
     _articles = _articleIDList;
   }
 
@@ -21,8 +19,6 @@ class SearchCommand extends GenericNotifier with BaseCommand {
   // ************************** INTERNALS
 
   List<String> _articles;
-
-  List<ArticlesModel> get _combinedInputList => OptionsModel.options();
 
   List<String> get _articleIDList {
     var articles = <String>[];
